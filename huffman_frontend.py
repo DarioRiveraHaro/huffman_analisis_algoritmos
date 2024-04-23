3e815a1f2bb83896d872835d383058151bd834d8
Dario Rivera Haro <dario.rivera6182@alumnos.udg.mx>
2024-04-22 22:03:14 -0600

    Comentarios
    Se agregaron comentarios a todo lo que es importarnte para el funcionamiento de la aplicacion.

--- huffman_frontend.py
+++ huffman_frontend.py
@@ -1,45 +1,49 @@
 import tkinter as tk
 from tkinter import filedialog
 
+# Abre un archivo seleccionado bajo el nombre 'archivo' en modo leer.
 def leer_archivo(archivo):
     with open(archivo, 'r') as archivo:
         contenido = archivo.read()
-        contar_carcteres(contenido)
-        
+        contar_carcteres(contenido) # Pasa como argumento el contenido del archivo.
+
+# Cuenta cada caracter y los guarda en diccionarios.
 def contar_carcteres(contenido):
     dic = {}
-    for caracter in contenido:
-        caracter = caracter.lower()
+    for caracter in contenido: # Recorre caracter por caracter
+        caracter = caracter.lower() # Convierte todos los caracteres en minusculas para no contar doble
         if caracter.isalpha() or caracter.isspace():  
-            if caracter in dic:
+            if caracter in dic: # Checa si el caracter en el que esta es uno que ya se guardo en un diccionario y le suma uno
                 dic[caracter] += 1
             else:
-                dic[caracter] = 1
+                dic[caracter] = 1 # Si no hay ningun caracter se crea el diccionario y se le agrega uno
     print(dic)
 
+# Frontend
 def ventana_main():
     root = tk.Tk()
     root.geometry('300x100')
     
+    # Cuando se llama esta funcion abre el file manager para poder selecionar el archivo
     def abrir_filemanager():
         archivo = filedialog.askopenfilename()
-        archivo_txt.delete(0, tk.END)
-        archivo_txt.insert(0, archivo)
-        leer_archivo(archivo)
+        archivo_entry.delete(0, tk.END)
+        archivo_entry.insert(0, archivo)
+        leer_archivo(archivo) # Pasa como argumento el archivo seleccionado 
 
-    main_frame = tk.Frame(master=root)
+    main_frame = tk.Frame(master=root) # Frame que contiene todo lo que hay en la ventana 
     main_frame.pack(padx=8, pady=8)
 
-    archivo_frame = tk.Frame(master=main_frame)
+    archivo_frame = tk.Frame(master=main_frame) # Frame que contiene el lable y la caja de texto sobre el archivo
     archivo_frame.pack(padx=8, pady=8)
 
     archivo_lbl = tk.Label(master=archivo_frame, text='Archivo: ')
     archivo_lbl.grid(column=0, row=0)
 
-    archivo_txt = tk.Entry(master=archivo_frame, width=35)
-    archivo_txt.grid(column=1, row=0)
+    archivo_entry = tk.Entry(master=archivo_frame, width=35)
+    archivo_entry.grid(column=1, row=0)
 
-    botones_frame = tk.Frame(master=main_frame)
+    botones_frame = tk.Frame(master=main_frame) # Frame que contiene todos los botones
     botones_frame.pack(padx=8, pady=8)
 
     examinar = tk.Button(master=botones_frame, text='Examinar', command=abrir_filemanager)
